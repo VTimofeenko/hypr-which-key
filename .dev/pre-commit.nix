04f6b0788d9cc84fa1c6ff629ee3c5e0ebda1135
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-18 17:32:53 -0700

    ci: work around clippy impurity

--- .dev/pre-commit.nix
+++ .dev/pre-commit.nix
@@ -1,4 +1,24 @@
-_: {
+{ pkgs, config, ... }:
+{
+  # Override hte environment for checking the flake in pure environment
+  # Source: https://github.com/cachix/git-hooks.nix/issues/452#issuecomment-2163017537
+  checks.pre-commit = pkgs.lib.mkForce (
+    let
+      drv = config.pre-commit.settings.run;
+    in
+    pkgs.stdenv.mkDerivation {
+      name = "pre-commit-run";
+      src = config.pre-commit.settings.rootSrc;
+      buildInputs = [
+        pkgs.git
+        pkgs.openssl
+        pkgs.pkg-config
+      ];
+      nativeBuildInputs = [ pkgs.rustPlatform.cargoSetupHook ];
+      cargoDeps = pkgs.rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
+      buildPhase = drv.buildCommand;
+    }
+  );
   settings = {
     hooks = {
       treefmt = {
