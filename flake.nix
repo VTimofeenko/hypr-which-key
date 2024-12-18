549eb9a10f363e3c058c06a854499ed66810ed02
Vladimir Timofeenko <id@vtimofeenko.com>
2024-12-18 09:05:25 -0800

    chore: unpin crane's nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,10 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
 
     devshell = {
       url = "github:numtide/devshell";
