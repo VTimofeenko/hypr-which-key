b8358591197407810ad4540d321d893661c76cd4
Vladimir Timofeenko <id@vtimofeenko.com>
2025-01-04 18:30:28 -0800

    WIP

--- .dev/devshells.nix
+++ .dev/devshells.nix
@@ -1,5 +1,5 @@
 { pkgs, config, ... }:
-{
+rec {
   default.packages = builtins.attrValues {
     inherit (pkgs)
       cargo
@@ -8,9 +8,20 @@
       gcc
       pkg-config
       bemenu
+      wayland
+      libxkbcommon
       ;
     inherit (pkgs.rustPackages) clippy;
 
     inherit (config.pre-commit.settings) package;
   };
+
+  default.env = [
+    {
+      name = "LD_LIBRARY_PATH";
+      value = builtins.foldl' (
+        a: b: "${a}:${b}/lib"
+      ) "${pkgs.vulkan-loader}/lib" default.packages;
+    }
+  ];
 }
--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,87 @@
-mod get_bindings;
-mod utils;
-use crate::get_bindings::{run_and_parse_command, ParsedBinding};
-use crate::utils::mod_mask_to_string;
+#![allow(warnings)]
+use hyprland::async_closure;
+use iced::futures;
+use iced::stream;
+use iced::widget::{self, button, center, column, row, scrollable, text, text_input};
+use iced::{color, Center, Element, Fill, Subscription, Task};
 
-use hyprland::event_listener::EventListener;
-// use hyprland::prelude::*;
-use std::process::{Child, Command, Stdio};
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+//use futures::channel::mpsc;
+use futures::sink::SinkExt;
+use futures::stream::{Stream, StreamExt};
 
-struct ShownBinding {
-    key_combo: String,
-    description: String,
+pub fn main() -> iced::Result {
+    iced::application("HyprWhichKey - Iced", HyprWhichKey::update, HyprWhichKey::view)
+        .theme(|_| iced::Theme::Dark)
+        .subscription(HyprWhichKey::subscription)
+        .run()
 }
 
-impl From<ShownBinding> for String {
-    fn from(value: ShownBinding) -> Self {
-        value.key_combo + " : " + &value.description
-    }
+#[derive(Default)]
+struct HyprWhichKey {
+    submap: String,
+    should_show: bool,
 }
 
-impl From<ParsedBinding> for ShownBinding {
-    fn from(value: ParsedBinding) -> Self {
+#[derive(Debug, Clone)]
+enum Message {
+    EnterSubmap(String),
+    ExitSubmap,
+}
+
+impl HyprWhichKey {
+    fn new() -> Self {
         Self {
-            key_combo: {
-                let mut result = mod_mask_to_string(value.modmask);
-                result.push(value.key);
-                result.join("+")
-            },
-            description: {
-                if value.description.is_empty() {
-                    [value.dispatcher.clone(), value.arg.clone()].join(" ")
-                } else {
-                    value.description.clone()
-                }
-            },
+            submap: "".to_string(),
+            should_show: false,
         }
     }
+
+    fn update(&mut self, message: Message) {
+        eprintln!("Got update: {:?}", message);
+        match message {
+            Message::EnterSubmap(submap) => {
+                self.should_show = true;
+                self.submap = submap;
+            }
+            Message::ExitSubmap => self.should_show = false,
+        };
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Subscription::run(listen)
+    }
+
+    fn view(&self) -> Element<Message> {
+        println!("View");
+        column![text(self.submap.clone()), text("BEPIS")].into()
+    }
 }
 
-#[derive(Debug)]
-enum Message {
-    Start(String),
-    Stop,
-}
+pub fn listen() -> impl Stream<Item = Message> {
+    let (tx, rx) = std::sync::mpsc::channel();
 
-fn main() {
-    let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
+    std::thread::spawn(move || {
         // Here be the event listener that connects to hyprland socket and sends the submap data
-        let mut listener = EventListener::new();
+        let mut listener = hyprland::event_listener::EventListener::new();
 
-        listener.add_sub_map_change_handler(move |data| {
+        listener.add_sub_map_changed_handler(move |data| {
             println!("Sending {data:#?}");
-            match data.is_empty() {
-                true => tx.send(Message::Stop).unwrap(),
-                false => {
-                    // let binds = Binds::get().unwrap();
-                    // using hyprctl to get binds since at the time of writing hyprland rust binds
-                    // don't have binds description
-                    //
-                    // Time to cheat using hyprctl binds that has it
-                    let binds = run_and_parse_command().unwrap();
 
-                    let message = binds
-                        .into_iter()
-                        .filter(|b| b.submap == data)
-                        // This looks ugly, but From
-                        // trait is not transitive?
-                        .map(|b| Into::<ShownBinding>::into(b).into())
-                        .collect::<Vec<String>>();
-
-                    tx.send(Message::Start(message.join("\n"))).unwrap()
-                }
-            };
+            tx.send(data.to_string()).unwrap()
         });
 
         listener.start_listener()
     });
 
-    let mut child: Option<Child> = None;
-
-    loop {
-        thread::sleep(Duration::from_secs(1));
-        match rx.recv() {
-            Ok(msg) => {
-                println!("{:?}", msg);
-                match msg {
-                    Message::Start(data) => {
-                        if child.is_none() {
-                            println!("Starting...");
-                            // FIXME: This is bad. "Echo" approach is used because bemenu needs
-                            // full stdin to render choices. I am not even using bemenu to select
-                            // anything at this point. Moreover using the bemenu as
-                            // selection/search would conflict with the actual shortcuts
-
-                            let first_command = Command::new("echo")
-                                .arg(data.clone())
-                                .stdout(Stdio::piped())
-                                .spawn()
-                                .expect("Failed to start first command");
-
-                            child = Some(
-                                Command::new("bemenu")
-                                    .arg("-l")
-                                    .arg("10")
-                                    .arg("--prompt")
-                                    .arg("")
-                                    .stdin(first_command.stdout.unwrap())
-                                    .spawn()
-                                    .expect("Failed to start subprocess"),
-                            );
-                        };
-                    }
-                    Message::Stop => {
-                        if let Some(mut c) = child.take() {
-                            println!("Stopping subprocess...");
-                            c.kill().expect("Failed to kill subprocess");
-                        }
-                    }
-                };
-            }
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                break;
-            }
+    stream::channel(100, move |mut output| async move {
+        loop {
+            async_std::task::sleep(std::time::Duration::from_secs(1)).await;
+            let foo = rx.recv().unwrap();
+            eprintln!("{foo:#?}");
+            match foo.is_empty() {
+                true => output.send(Message::ExitSubmap).await,
+                false => output.send(Message::EnterSubmap(foo)).await,
+            };
         }
-    }
+    })
 }
